c2c173595fec919d8ce06e6d6ff57afa88adac1d
Marwan <marwan.gendy@gmail.com>
2016-11-07 21:01:17 -0600

    conflict in lines 4 & 5 from file Class

--- Class.java
+++ Class.java
@@ -7,6 +7,8 @@ public class Class {
 
         System.out.println("updates line 3");
 
-        System.out.println("initial line 4");
+        System.out.println("dev 1 changes line 4");
+
+        System.out.println("added a different line 5 from dev1");
 
 }
