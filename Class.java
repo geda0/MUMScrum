7f5cf6a9471766804222ef37ca185310caabbba2
Marwan <marwan.gendy@gmail.com>
2016-11-07 20:28:38 -0600

    added Class2
    modified Class
    	Line 1 & 3 changed
    	Line 5 removed

--- Class.java
+++ Class.java
@@ -1,13 +1,12 @@
 public class Class {
      public static void main(String[] args) {
 
-        System.out.println("initial line 1");
+        System.out.println("updated line 1");
 
         System.out.println("initial line 2");
 
-        System.out.println("initial line 3");
+        System.out.println("updates line 3");
 
         System.out.println("initial line 4");
 
-        System.out.println("initial line 5");
 }
---
+++ Class2.java
@@ -0,0 +1,13 @@
+public class Class2 {
+     public static void main(String[] args) {
+
+        System.out.println("initial line 1");
+
+        System.out.println("initial line 2");
+
+        System.out.println("initial line 3");
+
+        System.out.println("initial line 4");
+
+        System.out.println("initial line 5");
+}
